6fdcdb0c334ffb7a491bfaf25c2e46abd02df73e
김찬우 <chanwoo219@gmail.com>
2025-05-21 19:25:45 +0900

    Add files via upload

---
+++ Project2/Project2/Project2App.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(title: "chevron.right") {
+                print("개발중...")
+            }
+        }
+    }
+}
